2daefeebb28c4c76ee2202eb6a567c8d3e189564
Yann <yann.berton@yahoo.com>
2024-06-10 18:34:32 +0200

    - Màj scripts étalonnage

--- Experimental/etalonnage_thermocouples/least_squares_method.py
+++ Experimental/etalonnage_thermocouples/least_squares_method.py
@@ -5,3 +5,20 @@ Created on Fri Jun  7 16:39:04 2024
 @author: yberton
 """
 
+# Import
+import numpy as np
+
+# List initialization
+L_theo = [] # List of theoretical/true values
+L_exp = [] # List of experimental values 
+
+# Functions' declaration
+y = lambda x,a,b : a*x + b # Linear function declaration
+theta = lambda Trk,Tk : Tk - Trk 
+
+
+
+# Variables of interest
+Trk = 20 # [°C]
+T = 20.27 # [°C]
+
