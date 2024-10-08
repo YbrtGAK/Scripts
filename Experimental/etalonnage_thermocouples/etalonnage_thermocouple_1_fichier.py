a9b90a05cf5ef5e8c44fc14a95f0b44a550b48af
Yann <yann.berton@yahoo.com>
2024-10-08 16:55:35 +0200

    Modification du script d'étalonnage (voir dernière note) Ajout des scripts jupyter

--- Experimental/etalonnage_thermocouples/etalonnage_thermocouple_1_fichier.py
+++ Experimental/etalonnage_thermocouples/etalonnage_thermocouple_1_fichier.py
@@ -13,18 +13,19 @@ import matplotlib.pyplot as plt  # Library for graphs
 import os
 import tkinter as tk  # Note : no mandotory, if you don't want to install tk package
 from tkinter import filedialog  # give the paths manually
-from utilities import getAFilesPath
+from utilities import getAFilesPath, getADirsPath, lvm_to_df, \
+    h5py_to_dataframe, getElementFromWidgetList,getAFilesPathToSave
 
 import numpy as np  # Library for vectorial objects
 # Library for machine learning tools
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import root_mean_squared_error
+from scipy import stats
+from sklearn.metrics import mean_squared_error
 from scipy.optimize import least_squares
 
-# Flags
 
+# Flags
 # Choose to display graphs
-disp_graph = True  # Display graph if true : not necessary for the calibration
+save_graph = True # Save the graph
 
 one_file_per_temp = False
 # If true, you need to have the following architecture :
@@ -42,19 +43,6 @@ main folder
 """
 # If false, just point to your csv file (or lvm, or txt)
 
-
-# File exploring function
-def getADirPath():
-    root = tk.Tk()
-    root.withdraw()
-
-    return(filedialog.askdirectory())
-
-#Get lvm file into dataframe
-def lvm_to_df(lvm_file_path):
-   return(pd.read_csv(lvm_file_path, sep='\t', on_bad_lines='skip',
-                     skiprows=22, decimal=','))
-
 #Visual check whether the right channels match the right sensors
 def check_sensor_channel_matching(df):
     df_std = df.std()
@@ -64,47 +52,62 @@ def check_sensor_channel_matching(df):
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
                          Get the data in DataFrames
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+path = getAFilesPath()
+fmt = path.split('/')[-1].split('.')[-1]
 
-df = lvm_to_df(getAFilesPath())
-df_std = check_sensor_channel_matching(df)
-df_filtered = df.filter(['T_PH_in (wall)','T_PH_surf2', 'T_PH_surf3', 'T_PH_surf4'])
-df = df_filtered
+match fmt :
+    case "h5" : 
+        df = h5py_to_dataframe(path,'Scan000','Detector000','NavAxes','Data0D',['CH00'])[0]
+    case "lvm" :
+        df = lvm_to_df(path)
+    case _ :
+        print("Format unrecognized, please check your file :/")
+        
+%matplotlib qt5
+df.plot()
+
+df_std = df.std()
+df_raw = df_filtered = df.copy()  # We save the raw value in a unbinded dataframe
+df_filtered = df.filter(getElementFromWidgetList("Keithley Channels", list(df_filtered.columns.values)))
 
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
                             Measures treatments
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-# The idea here is to get rid of the transition state in between a change of the calibration
-# bench's temperature of reference
-# In order to do so, a standart deviation calculus is calculated for each temperature value
-# over the four last values
+# The idea here is to get rid of the transition period in between two calibration points
+# In order to do so, a standart deviation calculus is realized for each temperature value over the four last values
 # If it is under a certain value to be defined by the user, then the measure is erased
 
 ###############Let's make sure that the steady state was reached###############
 
+#Get the serie time step
+dt = df.index[1] - df.index[0]
+dt = dt.seconds + dt.microseconds*10e-6 + dt.nanoseconds*10e-9 # [seconds]
+t_step = 40 # Time step for steady state conditions in the rolling(std) method of df [seconds]
+
 # Cut the experimental data corresponding to the end of the calibration
-%matplotlib qt5
-df.plot()
-df = df[df.index < 2250]
-df_raw = df.copy()  # We save the raw value in a unbinded dataframe
+
+df_filtered.plot()
+#df_filtered = df_filtered[df_filtered.index < 2250]
 
 # Get the temperature used for the calibration from user
 # Temperatures used for the calibration
-LT = [15, 30, 45, 60, 75, 90, 105, 120]
+LT = [-5 + i*15 for i in range(0,8)]
 
 # We find in between temp in order to split the dataframe in several ones for a giver reference temmperature
 LT_to_sort = [(LT[i] + LT[i+1])/2 for i in range(0, 7)]
+df_to_sort = df_filtered.copy()
 
-# Now we sort them. Here, the first column (corresponding to the "first" thermocouple)
+# Now we sort them. Here, the first column (cor ponding to the "first" thermocouple)
 # is used in order to split df. Make sure it is not broken before using it for this task.
 # It is broken ? Change the indice 0 with another one to avoid issues.
 
 Ldfs = []  # Initializing list of dataframe
 for Tmax in LT_to_sort:
-    df_left = df[df.iloc[:, 0] < Tmax]  # Cut data to add to the dataframe list
-    df = df[df.iloc[:, 0] >= Tmax]  # Cut this data from df
+    df_left = df_to_sort[df_to_sort.iloc[:, 0] < Tmax]  # Cut data to add to the dataframe list
+    df_to_sort = df_to_sort[df_to_sort.iloc[:, 0] >= Tmax]  # Cut this data from df
     Ldfs.append(df_left)  # Append the cut dataframe to the list of dataframe
 # Finally, append the remaing data in df as the last dataframe of the list
-Ldfs.append(df)
+Ldfs.append(df_to_sort)
 
 Ldf_treated = []  # Initialization of treated list of dataframes
 LTmean = []  # Initialization of Tmean array
@@ -114,7 +117,7 @@ for i in range(len(Ldfs)):  # Going through all the dataframes for each temperat
 
     # Let's make sure that the steady state was reached
     # Get a standart deviation for each value according 4 of its previous ones
-    df_std = df.rolling(4).std()
+    df_std = df.rolling(round(t_step/dt)).std()
     cols = [col for col in df_std.columns]  # get columns' names in a array
     # Create new cols names
     new_cols = [col + '_std' for col in df_std.columns]
@@ -125,7 +128,7 @@ for i in range(len(Ldfs)):  # Going through all the dataframes for each temperat
 
     # Delete the lines where std < 0.1 (why ? fixd after evaluation)
     for i in range(int(len(df.columns)/2), int(len(df.columns))):
-        df = df[df.iloc[:, i] < 0.1]
+        df = df[df.iloc[:, i] < 0.05]
 
     Tmean = []
     # Get the average value
@@ -137,7 +140,6 @@ for i in range(len(Ldfs)):  # Going through all the dataframes for each temperat
 
     # Add the mean temperature to a list
     LTmean.append(Tmean)
-
 
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
                                 Calibration
@@ -153,148 +155,93 @@ df_mean = pd.DataFrame(index=index, columns=columns, data=data)
 # Sort the dataframe per value
 df_mean.sort_index(axis=0, ascending=True, inplace=True)
 
-# Linear regression function
-
-
-def linear_regression(df_mean):
-    La, Lb, Lr_sq = [], [], []  # Empty list initialization
-    for col in df_mean.columns:
-        x = np.array(df_mean.index.values).reshape(
-            (-1, 1))  # Shape array for regression purposes
-        y = np.array(df_mean[col].values)  # Same
-        model = LinearRegression().fit(x, y)  # Linear regression
-        La.append(model.coef_)  # Get the slope coefficient (a)
-        Lb.append(model.intercept_)  # Get the coefficient of interception (b)
-        Lr_sq.append(model.score(x, y))  # Get the R² value
-    return(La, Lb, Lr_sq)
-
-
-# Linear regression for all column in df_mean
-La, Lb, Lr_sq = linear_regression(df_mean)
-
 # Fitting function
-
-
 def f_lin(x, a, b): return a*x + b
 
 # Least squared method
 
-
-def lst_sqrs(LTmean, LT, La, Lb):
+def lst_sqrs(LTmean, LT):
     Lnew_coeff = []
     ytheo = np.array(LT)
-    def fcost(coeff): return ytheo - coeff[0]*yexp - coeff[1]
+    def fcost(coeff): return ytheo - f_lin(yexp,coeff[0],coeff[1])
     for i in range(0, len(LTmean[0])):
         yexp = np.array([line[i] for line in LTmean])
-        # Exemple avec thermocouple 1
-        coeff_0 = np.array([La[i][0], Lb[i]])
-        res = least_squares(fcost, coeff_0)
-        Lnew_coeff.append((res.x[0], res.x[1], res.optimality))
+        coeff_0 = np.array([1, 0])
+        res = least_squares(fcost, coeff_0, method='trf')
+        Lnew_coeff.append((res.x[0], res.x[1]))
     return(Lnew_coeff)
 
-Lnew_coeff = lst_sqrs(LTmean, LT, La, Lb)
+Lnew_coeff = lst_sqrs(LTmean, LT)
 
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
                                Display graphs 
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-%matplotlib qt5
-if disp_graph:
-    textstr = []  #Initialization of the labels' list
-    T_fit = []  #Initialization of the fitted temperature list
-    L_RMSQ = [] #
-    L_RMSQ_lst = [] #Initialization of the Root Mean Squared list
+#%matplotlib qt5
+
+textstr = []  #Initialization of the labels' list
+T_fit = []  #Initialization of the fitted temperature list
+L_RMSE = [] #Initialization of the Root Mean Squared Error list
+
+#Horizontal axis to plot the fitted function's curve
+T_abs_fit = np.linspace(df_mean.index[0], df_mean.index[-1], 100)
+
+# For each thermocouple, get the fitted values from the evaluation of T_abs_fit
+for i in range(len(Lnew_coeff)):
     
-    #Horizontal axis to plot the fitted function's curve
-    T_abs_fit = np.linspace(df_mean.index[0], df_mean.index[-1], 100)
+    # Evaluation
+    T_fit.append(f_lin(T_abs_fit, Lnew_coeff[i][0], Lnew_coeff[i][1]))
+    L_RMSE.append(mean_squared_error(T_abs_fit,T_fit[-1], squared=False))
+    if Lnew_coeff[i][1] <= 0:  # Cases for esthetic display purposes
+        textstr.append('\n'.join((r'$T_{pred}$ = %.5f.$T_{mes}$ - %.5f ' % (Lnew_coeff[i][0], abs(Lnew_coeff[i][1])),  # Regression label creation
+                                  'RMSE = %.3e [°C]' % (L_RMSE[i]))))
+    else:
+        textstr.append('\n'.join((r'$T_{pred}$ = %.5f.$T_{mes}$ + %.5f ' % (Lnew_coeff[i][0], Lnew_coeff[i][1]),  # Regression label creation
+                                  'RMSE = %.3e [°C]' % (L_RMSE[i]))))
 
-    # For each thermocouple, get the fitted values from the evaluation of T_abs_fit
-    for i in range(len(Lnew_coeff)):
-        
-        # Evaluation
-        T_fit.append(f_lin(T_abs_fit, Lnew_coeff[i][0], Lnew_coeff[i][1]))
-        L_RMSQ_lst.append(root_mean_squared_error(T_abs_fit,T_fit[-1]))
-        L_RMSQ.append(root_mean_squared_error(T_abs_fit, f_lin(T_abs_fit,La[i][0], Lb[i])))
-        if Lnew_coeff[i][1] <= 0:  # Cases for esthetic display purposes
-            textstr.append('\n'.join((r'y = %.5f.x - %.5f ' % (Lnew_coeff[i][0], abs(Lnew_coeff[i][1])),  # Regression label creation
-                                      r'$\Delta T_{max}$ = %.2e [°C]' % (Lnew_coeff[i][2]))))
-        else:
-            textstr.append('\n'.join((r'y = %.5f.x + %.5f ' % (Lnew_coeff[i][0], Lnew_coeff[i][1]),  # Regression label creation
-                                      r'$\Delta T_{max}$ = %.2e [°C]' % (Lnew_coeff[i][2]))))
+# Display of the 3 temperatures in place
+phi = (1+5**0.5)/2  # Golden number to size graphs
+Tmax = df_mean.index[-1]  # For graph scaling purposes, get T max
 
-    # Display of the 3 temperatures in place
-    phi = (1+5**0.5)/2  # Golden number to size graphs
-    Tmax = df_mean.index[-1]  # For graph scaling purposes, get T max
+# 3 Subplots graph initialization
+if len(Lnew_coeff) % 2 == 0 : nrow = int(len(Lnew_coeff)/2)
+else : nrow = int(len(Lnew_coeff)/2) + 1
+fig1, axs = plt.subplots(
+    nrow, 2,constrained_layout=True, figsize=(12,12*phi))
 
-    # 3 Subplots graph initialization
-    
-    if len(Lnew_coeff) % 2 == 0 : nrow = int(len(Lnew_coeff)/2)
-    else : nrow = int(len(Lnew_coeff)/2) + 1
-    fig1, axs = plt.subplots(
-        nrow, 2)
+# State box proprieties to display the fitting function expression
+props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
 
-    # State box proprieties to display the fitting function expression
-    props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
-    
-    # Color list
-    Lcolor = ['b','r','g','purple','orange','pink', 'brown']
+# Color list
+Lcolor = ['b','r','g','purple','orange','pink', 'brown', 'yellow', 'blue']
 
-    for i in range(len(Lnew_coeff)):
-        if i%2 == 0 :
-            irow = int(i/2)
-            icol = 0
-        else :
-            icol = 1
-            irow = i - int(i/2) - 1
-        axs[irow][icol].scatter(df_mean.index, df_mean.iloc[:, i],
-                          color=Lcolor[i], label=df_mean.columns[i])
-        axs[irow][icol].plot(T_abs_fit, T_fit[i], color=Lcolor[i], label='Trend line')
-        axs[irow][icol].text(0.55, 0.1, textstr[i], transform=axs[irow][icol].transAxes, fontsize=10,
-                       verticalalignment='baseline', bbox=props)
-    # Action to do for all subplots/thermocouple analysis
-    for ax in axs:
-        for a in ax:
-            a.set_xlabel('Targeted temperature [°C]')  # Set x label
-            a.set_ylabel('Measured temperature [°C]')  # Set y label
-            a.plot(df_mean.index, df_mean.index, '--k', linewidth=2,
-                   label=r'$id_E$')  # Display identity function's curve
-            a.legend()  # Display legend
-            a.grid()  # Display a grid
-
-
-"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-                                Laboratory
-"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-ax = plt.subplot()
-
-ax.plot(df_mean.index, df_mean.index, '--k', linewidth=1,
-       label=r'$id_E$')
-
-f_linreg = f_lin(np.linspace(15,120,8), La[0][0], Lb[0])
-f_lstq= f_lin(np.linspace(15,120,8), Lnew_coeff[0][0], Lnew_coeff[0][1])
-
-ax.scatter(np.linspace(15,120,8), f_linreg,
-                  color='b', label="Linear regression", s=12)
-ax.scatter(np.linspace(15,120,8), f_lstq,
-                  color='r', label="Least squares", s=12)
-
-"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-                                Laboratory2
-"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-
-df_test = Ldfs[0][Ldfs[0].iloc[:,4] < 0.2]
-df_test.sort_values(ascending=True, axis=0,by=["T_PH_in (wall)"],inplace=True)
-df_test.reset_index(drop=True, inplace=True)
-for col in df_test :
-    df_test[col] -= 15
-
-fig2, axs = plt.subplots(2,2)
-for x in range(0,4) :
-    a = int(x/2)
-    if (x % 2) == 0 : 
-        b = 0
+for i in range(len(Lnew_coeff)):
+    if i%2 == 0 :
+        irow = int(i/2)
+        icol = 0
     else :
-        b = 1
-    axs[a][b].boxplot(df_test.iloc[:,x])
-    axs[a][b].set_title(df_test.columns[x])
-fig.suptitle("Dispersion des erreurs des thermocouples ")
-plt.show()
+        icol = 1
+        irow = i - int(i/2) - 1
+    axs[irow][icol].scatter(df_mean.index, df_mean.iloc[:, i],
+                      color=Lcolor[i], label= r'$T_{mes}$ (' +df_mean.columns[i] + ')')
+    axs[irow][icol].plot(T_abs_fit, T_fit[i], color=Lcolor[i], label=r'$T_{corrected}$')
+    axs[irow][icol].text(0.55, 0.1, textstr[i], transform=axs[irow][icol].transAxes, fontsize=10,
+                   verticalalignment='baseline', bbox=props)
+# Action to do for all subplots/thermocouple analysis
+for ax in axs:
+    for a in ax:
+        a.set_xlabel('Targeted temperature [°C]')  # Set x label
+        a.set_ylabel('Measured temperature [°C]')  # Set y label
+        a.plot(df_mean.index, df_mean.index, '--k', linewidth=2,
+               label=r'$id_E$')  # Display identity function's curve
+        a.legend()  # Display legend
+        a.grid()  # Display a grid
+
+if save_graph : 
+    fig1.savefig(getAFilesPathToSave())
+
+La = [coeff[0] for coeff in Lnew_coeff]
+Lb = [coeff[1] for coeff in Lnew_coeff]
+
+data = {'a' : La, 'b' : Lb, 'RMSE [°C]' : L_RMSE}
+df_fit = pd.DataFrame.from_dict(data, orient="index",columns=df_mean.columns).T
+df_fit.to_csv(getAFilesPathToSave(), sep=';', header=True)
